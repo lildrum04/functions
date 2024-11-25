ac05a03fb384fb36bd31b24261a01d039f80513f
lildrum04 <2319344@brunel.ac.uk>
2024-11-25 12:52:48 +0000

    dictionary use

---
+++ ClassLesson4.py
@@ -0,0 +1,34 @@
+# forest_dict = {'West':"You see a pair of shiny GoldenDaggers",
+#                'North':"You see a leather bag with BluePotions inside",
+#                'South':"On top of dead trunk, you see a GoldenFeathers",
+#                'East':"On top of a table you see a ManuScript"}
+#
+# item_dict = {'GoldenDaggers': 0,
+#              'BluePotions': 0,
+#              'GoldenFeathers': 0,
+#              'ManuScript': 0}
+#
+# skills_dict = {'Fly ability': 0,
+#                'Stealth': 0}
+#
+#
+# while True:
+#     player_choice =input("welcome to the magical forest, where would you like to go:'West','North','South','East'? ")
+#     if player_choice in forest_dict:
+#         print(forest_dict[player_choice])
+#         for j in forest_dict[player_choice].split():
+#             if j in item_dict:
+#                 if j == 'GoldenFeathers':
+#                     skills_dict['Fly ability'] += 1a
+#                     item_dict[j] += 1
+#                 elif j  == 'BluePotions':
+#                     skills_dict['BluePotions'] += 1
+#                     item_dict[j] += 1
+#                 else:
+#                     item_dict[j] += 1
+#         print(item_dict,skills_dict)
+#
+#     else : print("Please enter a valid player choice")
+#
+#
+
